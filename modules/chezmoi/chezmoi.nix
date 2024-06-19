5efa5045e3560695f627f85c98fe351a906091ce
toto <toto@mail.com>
2024-06-19 02:35:39 +0200

    chezmoi wip autosetup

--- modules/chezmoi/chezmoi.nix
+++ modules/chezmoi/chezmoi.nix
@@ -1,40 +1,24 @@
 { config, pkgs, ... }:
 
-#TODO: make a template variable that get applied
-#make the repo vabriable global
 let
-  chezmoiSetupScript = pkgs.writeShellScriptBin "chezmoi-setup" ''
-    #!/bin/bash
-
-    # URL to your chezmoi dotfiles repository
-    REPO_URL="https://github.com/PaysanCorrezien/dotfiles"
-
-    # Check if chezmoi is already set up
-    if [ -d "$HOME/.config/chezmoi" ]; then
-      echo "chezmoi is already set up. Applying changes..."
-      chezmoi update
-    else
-      echo "chezmoi is not set up. Initializing from $REPO_URL..."
-      chezmoi init --apply $REPO_URL
-    fi
-  '';
+  chezmoiSetupScript = pkgs.writeShellScriptBin "chezmoi-setup" (builtins.readFile ./chezmoi.sh);
 in
 {
-  environment.systemPackages = with pkgs; [
+  home.packages = with pkgs; [
     chezmoi
     chezmoiSetupScript
   ];
 
-  systemd.services.chezmoi-setup = {
-    description = "Chezmoi Setup Service";
-    wantedBy = [ "default.target" ];
-    after = [ "network.target" ];
-    serviceConfig = {
-      ExecStart = "${chezmoiSetupScript}/bin/chezmoi-setup";
+  systemd.user.services."chezmoi-setup" = {
+    Unit = { Description = "Chezmoi Setup"; };
+    Service = {
       Type = "oneshot";
+      ExecStart = "${chezmoiSetupScript}/bin/chezmoi-setup";
+    };
+    Install = {
+      WantedBy = [ "default.target" ];
+      After = [ "network.target" ];
     };
   };
-
-  systemd.services.chezmoi-setup.path = [ pkgs.chezmoi ];
 }
 
