190e31315a3001ab433a9e2cc74db7e9eef05c7b
Dylan Meunier <dylan19170@gmail.com>
2024-07-09 01:21:28 +0200

    using fetchigtuub and rust build for nixj🔥
    Issue-Reference:
    Signed-off-by: Dylan Meunier

--- modules/home-manager/programs/keybswitch.nix
+++ modules/home-manager/programs/keybswitch.nix
@@ -1,46 +1,46 @@
-{ config, pkgs, lib, ... }:
+{ config, lib, pkgs, ... }:
 
 let
-  keybswitchRepo = "https://github.com/PaysanCorrezien/keybswitch";
-  keybswitchBinaryPath = "/home/${config.home.username}/repo/keybswitch/target/release/keybswitch";
+  keybswitch = pkgs.rustPlatform.buildRustPackage rec {
+    pname = "keybswitch";
+    version = "unstable-2024-07-08";
 
-  keybswitchScript = pkgs.writeShellScriptBin "keybswitch" ''
-    # Start keybswitch binary
-    ${keybswitchBinaryPath}
-  '';
+    src = pkgs.fetchFromGitHub {
+      owner = "PaysanCorrezien";
+      repo = pname;
+      rev = "c35b144b4f62f1ed8d161aa880c9d7bba58f7288";
+      sha256 = "bdAsASlqgwErbxaITOfscaPBQfyykWDqDe6rjr0rX58=";
+    };
+
+    cargoSha256 = "jaTMT9GkWyPXFb2Seuk6bD1uPgW8jHeAgEnQUnM9/Mk=";
+
+    nativeBuildInputs = [
+      pkgs.pkg-config
+      pkgs.systemd  # systemd provides libudev
+      pkgs.openssl
+      pkgs.libffi
+    ];
+
+    # Explicitly set PKG_CONFIG_PATH
+    PKG_CONFIG_PATH = "${pkgs.systemd.dev}/lib/pkgconfig:${pkgs.openssl.dev}/lib/pkgconfig:${pkgs.libffi.dev}/lib/pkgconfig";
+
+    meta = {
+      description = "USB Keyboard Detection and Layout Switch";
+      homepage = "https://github.com/PaysanCorrezien/keybswitch";
+      license = lib.licenses.mit;
+      maintainers = with lib.maintainers; [ ];
+    };
+  };
 in
 {
-  # Create the .desktop file in autostart directory
-  home.file.".config/autostart/keybswitch.desktop".text = ''
+  home.packages = [ keybswitch ];
+
+  home.file.".config/autostart/keybswitch.desktop".text = lib.mkIf pkgs.stdenv.isLinux ''
     [Desktop Entry]
     Type=Application
-    Exec=${keybswitchBinaryPath}
-    Hidden=true
-    NoDisplay=true
+    Exec=${keybswitch}/bin/keybswitch > /home/${config.home.username}/keybswitch.log 2>&1
     X-GNOME-Autostart-enabled=true
     Name=Keybswitch
     Comment=USB Keyboard Detection and Layout Switch
   '';
-
-  # Ensure the binary is executable
-  home.activation = {
-    keybswitch = {
-      run = ''
-        chmod +x ${keybswitchBinaryPath}
-      '';
-    };
-
-    # Fetch from GitHub and build if the binary is not present
-    fetchAndBuildKeybswitch = lib.mkIf (!builtins.pathExists keybswitchBinaryPath) {
-      run = ''
-        echo "Fetching and building keybswitch..."
-        git clone ${keybswitchRepo} /tmp/keybswitch
-        cd /tmp/keybswitch
-        cargo build --release
-        mkdir -p $(dirname ${keybswitchBinaryPath})
-        cp target/release/keybswitch ${keybswitchBinaryPath}
-        chmod +x ${keybswitchBinaryPath}
-      '';
-    };
-  };
 }
