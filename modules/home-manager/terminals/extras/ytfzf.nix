c4143612367feff01399bd995aeaaff7e0314238
Dylan Meunier <dylan19170@gmail.com>
2024-07-22 00:54:59 +0200

    tui yt amazing client

--- modules/home-manager/terminals/extras/ytfzf.nix
+++ modules/home-manager/terminals/extras/ytfzf.nix
@@ -1,15 +1,21 @@
 { settings, pkgs, lib, ... }:
 
-# install only if the setings is set to true
 let cfg = settings.terminal.extras;
 in {
-  config =
-    lib.mkIf cfg.enable { home.packages = with pkgs; [ jq curl yt-fzf ]; };
-  programs.zsh.shellAliases = {
-    yta = "ytfzf -m --sort-by=relevance -t -l --pages=3"; # search for music
-    ytr =
-      "ytfzf --upload-date=week --sort-by=relevance -t -l "; # search for videos uploaded this week
-    yt = "ytfzf --sort-by=relevance -t -l --pages=3"; # general search
-    # TODO: add more aliases for other cusxtom searches ?
+  # Only apply configurations if enabled
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [ jq curl ytfzf ];
+
+    programs.zsh = {
+      shellAliases = {
+        yta = "ytfzf -m --sort-by=relevance -t -l --pages=3"; # search for music
+        ytr =
+          "ytfzf --upload-date=week --sort-by=relevance -t -l"; # search for videos uploaded this week
+        yt = "ytfzf --sort-by=relevance -t -l --pages=3"; # general search
+        yth =
+          "$HOME/.config/scripts/parse_watch_history.sh"; # view watch history , custom fn
+      };
+    };
   };
 }
+
