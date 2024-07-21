9c0cb36070c5bd7540f55efc7fe5f5326510771a
Dylan Meunier <dylan19170@gmail.com>
2024-07-22 00:52:10 +0200

    more music client test

--- modules/system/gui/extra/audio.nix
+++ modules/system/gui/extra/audio.nix
@@ -3,7 +3,13 @@
 let cfg = config.settings.isServer;
 in {
   config = lib.mkIf (!cfg) {
-    environment.systemPackages = with pkgs; [ termusic yt-dlp ];
+    environment.systemPackages = with pkgs; [
+      termusic
+      yt-dlp
+      cmus
+      mpv
+      musikcube
+    ];
   };
 }
 
