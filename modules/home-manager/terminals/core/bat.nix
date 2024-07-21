84ba3d01c2527a0fe9b0f4fc7a067cfbb7bc63c5
Dylan Meunier <dylan19170@gmail.com>
2024-07-22 00:55:55 +0200

    workaround for bat and rg perm errof + aliases

--- modules/home-manager/terminals/core/bat.nix
+++ modules/home-manager/terminals/core/bat.nix
@@ -1,19 +1,27 @@
 { config, pkgs, ... }: {
   programs.bat = {
     enable = true;
-
     config = { style = "auto,header-filesize"; };
-
     extraPackages = with pkgs.bat-extras; [
       batdiff
       batgrep
       batman
       batpipe
       batwatch
-      prettybat
     ];
   };
 
-  home.shellAliases = { cat = "${pkgs.bat} --style=plain"; };
-}
+  home.shellAliases = {
+    cat = "${config.home.profileDirectory}/bin/bat --style=plain";
+    man = "${config.home.profileDirectory}/bin/batman";
+    diff = "${config.home.profileDirectory}/bin/batdiff";
+    watch = "${config.home.profileDirectory}/bin/batwatch";
+  };
 
+  programs.zsh.initExtra = ''
+    export LESSOPEN="|${config.home.profileDirectory}/bin/batpipe %s"
+    export LESS="$LESS -R"
+    export BATPIPE="color"
+    unset LESSCLOSE
+  '';
+}
--- modules/home-manager/terminals/core/ripgrep.nix
+++ modules/home-manager/terminals/core/ripgrep.nix
@@ -1,31 +1,21 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}:
-{
-    programs.ripgrep = {
-      enable = true;
-      package = pkgs.ripgrep;
+{ config, lib, pkgs, ... }: {
+  programs.ripgrep = {
+    enable = true;
+    package = pkgs.ripgrep;
 
-      arguments = [
-        # Don't have ripgrep vomit a bunch of stuff on the screen
-        # show a preview of the match
-        "--max-columns=150"
-        "--max-columns-preview"
+    arguments = [
+      # Don't have ripgrep vomit a bunch of stuff on the screen
+      # show a preview of the match
+      "--max-columns=150"
+      "--max-columns-preview"
 
-        # ignore git files
-        "--glob=!.git/*"
+      # ignore git files
+      "--glob=!.git/*"
 
-        "--smart-case"
-      ];
-    };
-#
-    home.shellAliases = {
-      grep = " ${
-        pkgs.ripgrep
-      }";
-    };
+      "--smart-case"
+    ];
+  };
+  home.shellAliases = { grep = "${config.home.profileDirectory}/bin/rg"; };
+
 }
 
