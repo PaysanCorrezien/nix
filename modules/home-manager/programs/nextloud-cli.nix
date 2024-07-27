b1e72b8d7559f841d2306496d9bc8ab745bd3501
Dylan Meunier <dylan19170@gmail.com>
2024-07-27 11:53:20 +0200

    make secrets not block new install

--- modules/home-manager/programs/nextloud-cli.nix
+++ modules/home-manager/programs/nextloud-cli.nix
@@ -1,28 +1,38 @@
-{inputs,lib, config, pkgs,sops-nix, ... }:
+{ inputs, lib, config, pkgs, sops-nix, ... }:
 
 # TODO: sops url + name 
 # prompt for creds on install or launch the CLI
 # force sync with cmd or 
 # dont use readfile and make it work more directly
- # Import the sops module to ensure it can be used here
+# Import the sops module to ensure it can be used here
 
 # Import the sops module to ensure it can be used here
 let
+
+  #FIXME: This is a workaround to ensure that the secrets are read from the correct location
+  # this doestn work on first init i need to find a better way to do this but 
+  # i cant make the sops.templates work passed from system nixosmodules as of now
+  readSecretFile = file:
+    lib.optionalString (builtins.pathExists file) (builtins.readFile file);
   # NOTE: workaround because Home Manager doesn't seem to be able to read config.sops.secrets.nextcloudUrl if defined by global module?
-  nextcloudUrl = builtins.readFile "/run/secrets/nextcloudUrl";
-  nextcloudUser = builtins.readFile "/run/secrets/nextcloudUser";
+  nextcloudUrl = readSecretFile "/run/secrets/nextcloudUrl";
+  nextcloudUser = readSecretFile "/run/secrets/nextcloudUser";
   passwordLocalPath = "${config.home.homeDirectory}/Documents/Password";
   passwordRemotePath = "/Password";
   wallpaperLocalPath = "${config.home.homeDirectory}/Images/Wallpaper";
-  wallpaperRemotePath= "/Configuration/wallpaper";
+  wallpaperRemotePath = "/Configuration/wallpaper";
   musicLocalPath = "${config.home.homeDirectory}/Documents/Music";
   musicRemotePath = "/Musique";
   projetLocalPath = "${config.home.homeDirectory}/Documents/Projets";
   projetRemotePath = "/Work/Projet"; # Fixed spelling from 'projetRemotPath'
-  nextcloudExclude = builtins.readFile ./nextcloud_exclude.txt ; #gitignore for nextcloud
-  excludeFile = "${config.home.homeDirectory}/.config/Nextcloud/sync-exclude.lst";
-  stateFile = "${config.home.homeDirectory}/.config/Nextcloud/.nextcloud_cfg_written";
-  nextcloudLocal = "${config.home.homeDirectory}/.config/Nextcloud/nextcloud.cfg";
+  nextcloudExclude =
+    builtins.readFile ./nextcloud_exclude.txt; # gitignore for nextcloud
+  excludeFile =
+    "${config.home.homeDirectory}/.config/Nextcloud/sync-exclude.lst";
+  stateFile =
+    "${config.home.homeDirectory}/.config/Nextcloud/.nextcloud_cfg_written";
+  nextcloudLocal =
+    "${config.home.homeDirectory}/.config/Nextcloud/nextcloud.cfg";
 
   nextcloudConfig = ''
     [General]
@@ -62,9 +72,7 @@ let
     0\\Folders\\4\\version=2
   '';
 in {
-  home.packages = with pkgs; [
-    nextcloud-client
-  ];
+  home.packages = with pkgs; [ nextcloud-client ];
   home.file."${excludeFile}".text = nextcloudExclude; # gitignore file write
 
   home.activation.copyNextcloudConfig = lib.mkAfter ''
@@ -92,28 +100,28 @@ in {
     fi
   '';
 }
-  # The systemd service and timer definitions can be uncommented and adjusted as needed
-  # systemd.user.services.nextcloud-autosync = {
-  #   Unit = {
-  #     Description = "Auto sync Nextcloud";
-  #     After = "network-online.target"; 
-  #   };
-  #   Service = {
-  #     Type = "simple";
-  #     Environment = "nextcloud_key=${nextcloudKey} nextcloud_url=${nextcloudUrl} nextcloud_user=${nextcloudUser}";
-  #     ExecStart = "${pkgs.nextcloud-client}/bin/nextcloudcmd -h -n --user ${nextcloudUser} --password ${nextcloudKey} --path $HOME/Documents/Music/ ${nextcloudUrl}/remote.php/webdav/Musique && ${pkgs.nextcloud-client}/bin/nextcloudcmd -h -n --user ${nextcloudUser} --password ${nextcloudKey} --path $HOME/Documents/Password/ ${nextcloudUrl}/remote.php/webdav/Password";
-  #     TimeoutStopSec = "180";
-  #     KillMode = "process";
-  #     KillSignal = "SIGINT";
-  #   };
-  #   Install.WantedBy = ["multi-user.target"];
-  # };
+# The systemd service and timer definitions can be uncommented and adjusted as needed
+# systemd.user.services.nextcloud-autosync = {
+#   Unit = {
+#     Description = "Auto sync Nextcloud";
+#     After = "network-online.target"; 
+#   };
+#   Service = {
+#     Type = "simple";
+#     Environment = "nextcloud_key=${nextcloudKey} nextcloud_url=${nextcloudUrl} nextcloud_user=${nextcloudUser}";
+#     ExecStart = "${pkgs.nextcloud-client}/bin/nextcloudcmd -h -n --user ${nextcloudUser} --password ${nextcloudKey} --path $HOME/Documents/Music/ ${nextcloudUrl}/remote.php/webdav/Musique && ${pkgs.nextcloud-client}/bin/nextcloudcmd -h -n --user ${nextcloudUser} --password ${nextcloudKey} --path $HOME/Documents/Password/ ${nextcloudUrl}/remote.php/webdav/Password";
+#     TimeoutStopSec = "180";
+#     KillMode = "process";
+#     KillSignal = "SIGINT";
+#   };
+#   Install.WantedBy = ["multi-user.target"];
+# };
 
-  # systemd.user.timers.nextcloud-autosync = {
-  #   Unit.Description = "Automatic sync files with Nextcloud every hour";
-  #   Timer.OnBootSec = "5min";
-  #   Timer.OnUnitActiveSec = "60min";
-  #   Install.WantedBy = ["multi-user.target" "timers.target"];
-  # };
+# systemd.user.timers.nextcloud-autosync = {
+#   Unit.Description = "Automatic sync files with Nextcloud every hour";
+#   Timer.OnBootSec = "5min";
+#   Timer.OnUnitActiveSec = "60min";
+#   Install.WantedBy = ["multi-user.target" "timers.target"];
+# };
 
-  # systemd.user.startServices = true;
+# systemd.user.startServices = true;
