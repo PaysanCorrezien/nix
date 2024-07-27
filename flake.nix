dcc084efaeb3f897bfe63ec456cd24c3b996d4cb
Dylan Meunier <dylan19170@gmail.com>
2024-07-27 16:17:13 +0200

    g

--- flake.nix
+++ flake.nix
@@ -1,6 +1,5 @@
 {
   description = "NixOS configuration";
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
@@ -19,33 +18,26 @@
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    # TODO:
-    # stylix = {
-    #   url = "github:danth/stylix";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
   };
 
-  # TODO : move computer conf on /machine/ subfolder
-  # TEST: serv conf
-  outputs = { disko, self, nixpkgs, home-manager, sops-nix, ... }@inputs:
-    # TODO: make this a varaible too
-    let system = "x86_64-linux";
+  outputs = { self, nixpkgs, home-manager, sops-nix, disko, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
     in {
       nixosConfigurations = {
         lenovo = nixpkgs.lib.nixosSystem {
+          inherit system;
           specialArgs = { inherit inputs nixpkgs; };
-          modules = let
-            pkgs = import nixpkgs {
-              system = "x86_64-linux";
-              config.allowUnfree = true;
-            };
-          in [
+          modules = [
             ./global-default.nix
             ./disko.nix
             ./hosts/lenovo.nix
             disko.nixosModules.disko
+            home-manager.nixosModules.home-manager
             ({ config, pkgs, lib, ... }: {
               nixpkgs.hostPlatform = system;
               imports = lib.optional
@@ -54,60 +46,17 @@
             })
           ];
         };
-        # Add this new output for disko
-        diskoConfigurations.default = let
-          evalNixos = nixpkgs.lib.nixosSystem {
-            inherit system;
-            modules = [
-              disko.nixosModules.disko
-              {
-                nixpkgs.hostPlatform = system;
-                disko.devices = import ./disko.nix { inherit (nixpkgs) lib; };
-              }
-            ];
-          };
-        in evalNixos.config.system.build.diskoScript;
       };
 
-      # # NOTE: placeholder work wsl need to be done quick
-      #         WSL = nixpkgs.lib.nixosSystem {
-      #         specialArgs = { inherit inputs nixpkgs globalDefaults; };
-      #         modules = let
-      #           pkgs = import nixpkgs {
-      #             system = globalDefaults.system;
-      #           };
-      #         in [
-      #           ./hosts/WSL.nix
-      #           ./modules/common.nix
-      #           ./dynamic-grub.nix
-      #           inputs.home-manager.nixosModules.home-manager
-      #           {
-      #             home-manager.users.dylan = import ./modules/home-manager/home.nix { inherit pkgs inputs; };
-      #           }
-      #         ];
-      #       };
-      #
-      # # NOTE: placeholder for any no x86_64-linux based system 
-      #       raspberryPi = nixpkgs.lib.nixosSystem {
-      #         specialArgs = { inherit inputs nixpkgs globalDefaults; };
-      #         modules = let
-      #           pkgs = import nixpkgs {
-      #             system = "aarch64-linux";
-      #           };
-      #         in [
-      #           # Add Raspberry Pi specific modules here if needed
-      #         ];
-      #       };
+      diskoConfigurations.default = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          disko.nixosModules.disko
+          {
+            nixpkgs.hostPlatform = system;
+            disko.devices = import ./disko.nix { inherit (nixpkgs) lib; };
+          }
+        ];
+      };
     };
-
-  # nixosConfigurations.homeserver = nixpkgs.lib.nixosSystem {
-  #   specialArgs = { inherit inputs; };
-  #   modules = [
-  #     ./homeserver.nix
-  #     ./dynamic-grub.nix  # Include the dynamic GRUB module
-  #     inputs.home-manager.nixosModules.home-manager
-  #   ];
-  # };
-
 }
-
