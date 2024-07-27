4f28dbeedcb590f46027a5d174192c7fafc65cc4
Dylan Meunier <dylan19170@gmail.com>
2024-07-27 23:39:50 +0200

    k

--- setup.sh
+++ setup.sh
@@ -9,7 +9,8 @@ set -e
 # Move hardware configuration to the expected location
 # mv /mnt/etc/nixos/hardware-configuration.nix /mnt/etc/nixos/
 
-sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko --flake github:paysancorrezien/nix#default --no-write-lock-file
+# sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko --flake github:paysancorrezien/nix#default --no-write-lock-file
+sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko --flake github:paysancorrezien/nix#diskoConfigurations.default --no-write-lock-file
 
 # sudo nixos-install --flake github:paysancorrezien/nix#lenovo --no-write-lock-file
 # nixos-install --flake github:paysancorrezien/nix#lenovo --no-write-lockfile --show-trace
