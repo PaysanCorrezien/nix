639ea075a1e736383141ba2cef297f8d94a596bc
Dylan Meunier <dylan19170@gmail.com>
2024-08-15 15:45:55 +0200

    z

--- modules/home-manager/terminals/extras/aichat.nix
+++ modules/home-manager/terminals/extras/aichat.nix
@@ -1,10 +1,27 @@
 { settings, pkgs, lib, ... }:
 let cfg = settings.terminal.extras;
-
 in {
   config = lib.mkIf cfg.enable {
-
     home.packages = with pkgs; [ aichat ];
+
+    programs.zsh = {
+      shellAliases.ai = "aichat";
+      # NOTE: this make ctrl / to trigger aichat to correct current prompt
+      # https://github.com/sigoden/aichat/blob/main/scripts/shell-integration/integration.zsh
+      initExtra = ''
+        aichat_zsh() {
+          if [[ -n "$BUFFER" ]]; then
+            local _old=$BUFFER
+            BUFFER+="⌛"
+            zle -I && zle redisplay
+            BUFFER=$(aichat -e "$_old")
+            zle end-of-line
+          fi
+        }
+        zle -N aichat_zsh
+        bindkey '^_' aichat_zsh
+      '';
+    };
 
     xdg.configFile = {
       "aichat/config.yaml".text = ''
@@ -16,4 +33,3 @@ in {
     };
   };
 }
-
