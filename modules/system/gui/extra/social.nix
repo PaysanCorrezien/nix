f7cc52cf74ab02b904411733ecdf993261286aa2
Dylan Meunier <dylan19170@gmail.com>
2024-08-20 20:10:29 +0200

    matrix upstream issue

--- modules/system/gui/extra/social.nix
+++ modules/system/gui/extra/social.nix
@@ -3,7 +3,9 @@
 let cfg = config.settings.social;
 in {
   config = lib.mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [ element-desktop discord ];
+    environment.systemPackages = with pkgs; [ 
+    # element-desktop
+    discord ];
   };
 }
 
