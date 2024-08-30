17af2b66e13f535aa76dc5a2d307acecbdb67926
Dylan Meunier <dylan19170@gmail.com>
2024-08-31 01:30:05 +0200

    again 💀

--- disko.nix
+++ disko.nix
@@ -1,6 +1,6 @@
-{config, lib, ... }:
-
+{ lib, ... }:
 let
+  # Expanded function to score drives based on type and performance expectations
   scoreDrive = drive:
     if lib.hasPrefix "nvme" drive then 100
     else if lib.hasPrefix "sd" drive then 80
@@ -13,47 +13,33 @@ let
     else if lib.hasPrefix "fd" drive then 10   # Floppy drives
     else 0; # Any other type of drive
 
-# potentialDrives = builtins.trace "Potential drives: ${builtins.toString potentialDrives}" (
+  # Find all available drives, excluding nvme0
+  availableDrives =
+    builtins.filter (d: builtins.pathExists ("/dev/" + d) && d != "nvme0")
+      (builtins.attrNames (builtins.readDir /dev));
 
-  allPossibleDrives = 
-    (map (i: "/dev/nvme${toString i}n1") (lib.range 0 7)) ++
-    (map (c: "/dev/sd${c}") (lib.stringToCharacters "abcdefghijklmnop")) ++
-    (map (c: "/dev/vd${c}") (lib.stringToCharacters "abcdefghijklmnop")) ++
-    (map (c: "/dev/xvd${c}") (lib.stringToCharacters "abcdefghijklmnop")) ++
-    (map (c: "/dev/hd${c}") (lib.stringToCharacters "abcdefghijklmnop")) ++
-    (map (i: "/dev/mmcblk${toString i}") (lib.range 0 9)) ++
-    (map (i: "/dev/loop${toString i}") (lib.range 0 7)) ++
-    (map (i: "/dev/sr${toString i}") (lib.range 0 3)) ++
-    (map (i: "/dev/fd${toString i}") (lib.range 0 3));
-
-  potentialDrives = lib.filter (drive: builtins.pathExists drive) allPossibleDrives;
-
-  debugPotentialDrives = builtins.trace "Potential drives: ${builtins.toString potentialDrives}" potentialDrives;
-
-
-  # Function to select the best drive
-  selectBestDrive = drives:
-  let
-    scoredDrives = map (drive: { name = drive; score = scoreDrive (lib.last (lib.splitString "/" drive)); }) drives;
-    sortedDrives = lib.sort (a: b: a.score > b.score) scoredDrives;
-  in
-  builtins.trace "Scored drives: ${builtins.toJSON scoredDrives}"
-    (if builtins.length sortedDrives > 0
-     then (builtins.head sortedDrives).name
-     else throw "No suitable drive found for installation. Please check your hardware configuration.");
-
+  # Find the best drive, considering all types
+  bestDrive = "/dev/" + lib.head (lib.sort (a: b: scoreDrive a > scoreDrive b)
+    (builtins.filter (d: 
+      lib.hasPrefix "nvme" d || 
+      lib.hasPrefix "sd" d || 
+      lib.hasPrefix "vd" d || 
+      lib.hasPrefix "xvd" d || 
+      lib.hasPrefix "hd" d || 
+      lib.hasPrefix "mmcblk" d
+    ) availableDrives));
 in
 {
   disko.devices = {
     disk = {
       main = {
         type = "disk";
-        device = lib.mkDefault (selectBestDrive debugPotentialDrives);
+        device = bestDrive;
         content = {
           type = "gpt";
           partitions = {
             ESP = {
-              name = "disk-main-ESP";
+              name = "ESP";
               size = "500M";
               type = "EF00";
               content = {
@@ -63,7 +49,6 @@ in
               };
             };
             root = {
-              name = "disk-main-root";
               size = "100%";
               content = {
                 type = "filesystem";
@@ -76,23 +61,15 @@ in
       };
     };
   };
-
-  # Filesystem configurations
+  # Override the conflicting fileSystems configurations
   fileSystems = {
     "/" = lib.mkForce {
-      device = "/dev/disk/by-partlabel/disk-main-root";
+      device = "/dev/disk/by-partlabel/root";
       fsType = "ext4";
     };
     "/boot" = lib.mkForce {
-      device = "/dev/disk/by-partlabel/disk-main-ESP";
+      device = "/dev/disk/by-partlabel/ESP";
       fsType = "vfat";
     };
   };
-
-  # Provide feedback on configuration
-  system.activationScripts.diskoReport = ''
-    echo "[Disko] Installation drive: $(readlink -f ${config.disko.devices.disk.main.device})"
-    echo "[Disko] Root partition: $(readlink -f /dev/disk/by-partlabel/disk-main-root)"
-    echo "[Disko] Boot partition: $(readlink -f /dev/disk/by-partlabel/disk-main-ESP)"
-  '';
 }
