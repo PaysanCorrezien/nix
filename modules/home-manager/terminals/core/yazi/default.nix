70a8ee48710745b996d1b3d2966e7f5f81e69d03
Dylan Meunier <dylan19170@gmail.com>
2024-09-12 23:23:43 +0200

    yazi no hidden

--- modules/home-manager/terminals/core/yazi/default.nix
+++ modules/home-manager/terminals/core/yazi/default.nix
@@ -1,10 +1,23 @@
 { lib, pkgs, config, ... }:
-
 let
 in {
-
   home.packages = with pkgs; [ miller ouch xdragon zoxide ueberzugpp ];
-
-  programs.yazi = { enable = true; };
+  
+  programs.yazi = {
+    enable = true;
+    
+    settings = {
+      manager = {
+        show_hidden = true;
+        sort_by = "modified";
+        sort_dir_first = true;
+        sort_reverse = false;
+      };
+      preview = {
+        max_width = 1920;
+        max_height = 1080;
+      };
+    };
+  };
 }
 
