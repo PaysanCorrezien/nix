f2fcd34c55b49dec88ffe83c3a981387c1df62da
Dylan Meunier <dylan19170@gmail.com>
2024-09-28 14:52:54 +0200

    rustscan quick scan alias

--- modules/home-manager/terminals/extras/default.nix
+++ modules/home-manager/terminals/extras/default.nix
@@ -1,9 +1,26 @@
 {settings,lib, config, pkgs, ... }:
-  let cfg = settings.terminal.extras;
+let 
+  cfg = settings.terminal.extras;
+  rustscanFullScript = pkgs.writeShellScriptBin "rustscanfull" ''
+    #!/usr/bin/env bash
+    if [ $# -eq 0 ]; then
+      echo "Usage: rustscanfull <IP address or CIDR>"
+      exit 1
+    fi
 
+    target="$1"
+    timestamp=$(date +%Y%m%d_%H%M%S)
+    sanitized_target=$(echo "$target" | tr '/' '_')
+    output_file="$HOME/''${sanitized_target}_$timestamp.txt"
+
+    echo "Scanning $target. Output will be written to $output_file"
+
+    rustscan -a "$target" --ulimit 10000 -- -sV -sC -Pn -oN "$output_file"
+
+    echo "Scan complete. Results written to $output_file"
+  '';
 in
 {
-
   imports = [
     ./btop.nix
     ./rust.nix
@@ -19,9 +36,15 @@ in
   ];
   config = lib.mkIf cfg.enable {
     home.packages = with pkgs; [
-  imagemagick
-  rustscan
-  ];
+      imagemagick
+      rustscan
+      rustscanFullScript
+    ];
+    programs.bash.shellAliases = {
+      rustscanfull = "rustscanfull";
+    };
+    programs.zsh.shellAliases = {
+      rustscanfull = "rustscanfull";
+    };
   };
 }
-
