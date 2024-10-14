9ad806676f7872b15366c576cd0a76dbad2e1331
Dylan Meunier <dylan19170@gmail.com>
2024-10-14 19:38:15 +0200

    fix: wrong aliases

---
+++ modules/network/ssh.nix
@@ -0,0 +1,51 @@
+{ inputs, config, pkgs, lib, ... }:
+let
+  cfg = config;
+in
+{
+  options.settings = lib.mkOption {
+    type = lib.types.submodule {
+      options.ssh = lib.mkOption {
+        type = lib.types.submodule {
+          options = {
+            enable = lib.mkEnableOption "Enable SSHD custom hardened configuration";
+          };
+        };
+        default = { enable = true; };
+        description = "SSHD settings";
+      };
+    };
+    default = { };
+  };
+  config = lib.mkIf (cfg.settings.ssh.enable) {
+    services.openssh = {
+      enable = true;
+      settings = {
+        PasswordAuthentication = false;
+        PermitRootLogin = "no";
+        KbdInteractiveAuthentication = false;
+        X11Forwarding = false;
+        AuthenticationMethods = "publickey";
+        UsePAM = false;
+        PermitEmptyPasswords = false;
+      };
+      extraConfig = ''
+        AllowUsers ${config.settings.username}
+        PubkeyAuthentication yes
+        AllowTcpForwarding no
+        AllowAgentForwarding no
+        MaxAuthTries 10
+        PermitUserEnvironment no
+        Protocol 2
+        Ciphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr
+        MACs hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,umac-128-etm@openssh.com,rsa-sha2-512,rsa-sha2-256
+        KexAlgorithms curve25519-sha256,curve25519-sha256@libssh.org,diffie-hellman-group-exchange-sha256
+      '';
+    };
+  };
+
+  users.users.${config.settings.username} = {
+    openssh.authorizedKeys.keyFiles =
+      [ "${inputs.self}/hosts/keys/${config.settings.hostname}.pub" ];
+  };
+}
--- modules/system/gui/extra/docker.nix
+++ modules/system/gui/extra/docker.nix
@@ -1,19 +1,18 @@
 { config, pkgs, lib, ... }:
 
 let
-  userName = "dylan";
-#TODO: rename this to docker / nvidia ?
- cfg = config.settings.docker.enable;
-
-in {
+  userName = "dylan"; #TODO: rename this to docker / nvidia ?
+  cfg = config.settings.docker.enable;
+in
+{
   config = lib.mkIf (cfg) {
     virtualisation.docker = {
       enable = true;
       enableOnBoot = true;
       logDriver = "json-file";
       extraOptions = ''
-      --log-opt max-size=10m --log-opt max-file=3
-    '';
+        --log-opt max-size=10m --log-opt max-file=3
+      '';
       rootless = {
         enable = true;
         setSocketVariable = true;
@@ -29,14 +28,10 @@ in {
       docker-compose
       lazydocker
     ];
+
+    environment.shellAliases = {
+      dku = "docker-compose up -d";
+      dkd = "docker-compose down";
     };
-        programs.zsh = {
-      shellAliases.dku = "docker-compose up -d";
-      shellAliases.dkd = "docker-compose down";
-        };
-
-    
-    #TODO: create alias for docker-compose tasks
+  };
 }
-
-
