b58db4e583cbaa9e76987e76f4cd4734dfb88850
Dylan Meunier <dylan19170@gmail.com>
2024-10-26 16:56:34 +0200

    fix: espanso on wayland

--- modules/system/gui/programs/espanso.nix
+++ modules/system/gui/programs/espanso.nix
@@ -1,8 +1,11 @@
-{ config, pkgs, lib, ... }:
-
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 let
   cfg = config.settings;
-# TEST: prepare for futur rebuild
   username = builtins.getEnv "USER";
   espansoConfigPath = "/home/${username}/.config/espanso/match/search.yml";
   espansoSetupScript = pkgs.writeShellScript "setup-espanso" ''
@@ -10,47 +13,38 @@ let
       echo "Espanso config found. Skipping setup."
       exit 0
     fi
-
     if ! ${pkgs.gh}/bin/gh auth status &>/dev/null; then
       echo "GitHub CLI not authenticated. Skipping setup."
       exit 0
     fi
-
     echo "Espanso config not found. Cloning from GitHub..."
     github_username=$(${pkgs.gh}/bin/gh api user -q .login)
     if [ -z "$github_username" ]; then
       echo "Failed to get GitHub username. Aborting."
       exit 1
     fi
-
     rm -rf /home/${username}/.config/espanso
     cd /home/${username}/.config
     ${pkgs.gh}/bin/gh repo clone "$github_username/espanso.git" espanso
   '';
 in
 {
+  imports = [
+    # CREDIT: https://github.com/NixOS/nixpkgs/issues/249364#issuecomment-2336599201
+    ./espanso-capdacoverride/default.nix
+  ];
+  # services.espanso.enable = false
   config = lib.mkIf (cfg.displayServer != null) {
-    services = lib.mkMerge [
-      (lib.mkIf (cfg.displayServer == "xorg") {
-        espanso = {
-          enable = true;
-          package = pkgs.espanso;
-          wayland = false;
-        };
-      })
-      (lib.mkIf (cfg.displayServer == "wayland") {
-        espanso = {
-          enable = true;
-          package = pkgs.espanso-wayland;
-          wayland = true;
-        };
-      })
-    ];
+    # Enable the capdacoverride capability for Wayland
+    programs.espanso.capdacoverride.enable = true;
+
+    services.espanso = {
+      enable = true;
+      package = config.programs.espanso.capdacoverride.package;
+    };
 
     boot.kernelModules = [ "uinput" ];
-
     environment.systemPackages = [ pkgs.gh ];
-
     system.activationScripts.setupEspanso = lib.stringAfter [ "users" ] ''
       ${espansoSetupScript}
     '';
