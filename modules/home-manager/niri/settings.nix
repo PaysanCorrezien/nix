cbfbf1b19f99b7c96e85360bea34a8a93a2f2714
Dylan Meunier <dylan19170@gmail.com>
2026-01-14 14:23:33 +0100

    cc

--- modules/home-manager/niri/settings.nix
+++ modules/home-manager/niri/settings.nix
@@ -5,6 +5,8 @@
 }:
 {
   config = lib.mkIf (settings.windowManager == "niri") {
+    settings.gnome.extra.enable = lib.mkForce false;
+    settings.hyprland.extra.enable = lib.mkForce false;
     home.file.".config/niri/config.kdl".source = ./config.kdl;
   };
 }
