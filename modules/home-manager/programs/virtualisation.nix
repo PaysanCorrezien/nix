e0eed117c3c669245996aa1533ebcbb55f532e61
Dylan Meunier <dylan19170@gmail.com>
2026-01-14 14:18:14 +0100

    niriarc2

--- modules/home-manager/programs/virtualisation.nix
+++ modules/home-manager/programs/virtualisation.nix
@@ -5,7 +5,7 @@ let cfg = settings.virtualisation.enable;
 in {
   # Enable virt-manager
 
-  config = lib.mkIf cfg {
+  config = lib.mkIf (cfg && settings.windowManager == "gnome") {
     dconf.settings = {
       "org/virt-manager/virt-manager" = { system-tray = true; };
       "org/virt-manager/virt-manager/connections" = {
